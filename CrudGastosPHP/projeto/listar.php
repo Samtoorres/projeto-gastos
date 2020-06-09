8907b07ee143010d646ce7239930a8d5fc11c94d
Samuel Torres <samtorres.v05@gmail.com>
2020-06-09 15:52:28 -0300

    Alteração Front-End

--- CrudGastosPHP/projeto/listar.php
+++ CrudGastosPHP/projeto/listar.php
@@ -15,7 +15,7 @@ $conectar->close();
 <!DOCTYPE html>
 <html>
     <head>   
-        <title>Login</title>
+        <title>Lista</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="../bootstrap-4.5.0-dist/css/style.css">
@@ -27,30 +27,30 @@ $conectar->close();
         <div class="wrapper">
             <div class="container">
                 <div class="col-lg-12 col-md-12 col-sm-12">
-				<form class="formulario" method="GET" action="conexao.php">
-                        <h2 class="form-heading text-center">Lista de Contatos</h2>
-						<table class="table table-striped">
-  	<thead class="thead-dark">
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">nome</th>
-      <th scope="col">numero</th>
-    </tr>
-  </thead>
-  <tbody>
-		<?php while ($dado = $buscar->fetch_array()) {?>
-		<tr>
-			<td><?php echo $dado["id"] ?></td>
-			<td><?php echo $dado["nome"] ?></td>
-			<td><?php echo $dado["numero"] ?></td>
-		</tr>
-		<?php }?>
-  
-  </tbody>
-</table>
-<a href="acesso.html"><button type="button" class="btn btn-lg btn-danger btn-block" value="Cancelar" onclick="redirecionamento();">Voltar</button></a>
-</form>
-	</div>
+                    <form class="formulario" method="GET" action="conexao.php">
+                        <h2 class="form-heading text-center">Lista de Gastos</h2>
+                        <table class="table table-striped">
+                            <thead class="thead-dark">
+                            <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">nome</th>
+                            <th scope="col">valor do gasto</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                <?php while ($dado = $buscar->fetch_array()) {?>
+                                <tr>
+                                    <td><?php echo $dado["id"] ?></td>
+                                    <td><?php echo $dado["nome"] ?></td>
+                                    <td><?php echo $dado["numero"] ?></td>
+                                </tr>
+                                <?php }?>
+                        
+                        </tbody>
+                        </table>
+                        <a href="acesso.html"><button type="button" class="btn btn-lg btn-danger btn-block" value="Cancelar" onclick="redirecionamento();">Voltar</button></a>
+                    </form>
+	            </div>
             </div>
         </div>
         <script src="bootstrap/js/jquery-3.4.1.min.js"></script>
