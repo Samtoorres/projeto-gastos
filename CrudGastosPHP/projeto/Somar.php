9af4c509e2fe7a520701374d81b7e6f1cf1b26c7
Samuel Torres <samtorres.v05@gmail.com>
2020-06-11 15:35:15 -0300

    Implementação funcionalidade somar gastos

--- CrudGastosPHP/projeto/Somar.php
+++ CrudGastosPHP/projeto/Somar.php
@@ -1,5 +1,56 @@
 <?php
 
-    echo"Testando";
+$nome_servidor = "localhost";
+$nome_usuario = "root";
+$senha = "";
+$nome_banco = "phpprojetov2";
 
+$conectar = new mysqli($nome_servidor, $nome_usuario, $senha,$nome_banco);
+
+$buscar = mysqli_query($conectar,"SELECT SUM(valor) AS Soma FROM gastos");
+
+$conectar->close();
 ?>
+<html>
+
+<!DOCTYPE html>
+<html>
+    <head>   
+        <title>Lista</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../bootstrap-4.5.0-dist/css/style.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap-4.5.0-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../bootstrap-4.5.0-dist/css/style.css">
+        <link rel="shortcut icon" href="bootstrap/img/favicon.ico" sizes="16x16">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="container">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <form class="formulario" method="GET" action="conexao.php">
+                        <h2 class="form-heading text-center">Soma dos Gastos</h2>
+                        <table class="table table-striped">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">Total dos gastos a pagar</th>
+                                </tr>
+                        </thead>
+                        <tbody>
+                             <?php while ($dado = mysqli_fetch_assoc($buscar)) {?>
+                                <tr>
+                                    <td><?php echo "R$ = ".$dado['Soma']?></td>
+                                </tr>
+                            <?php }?>
+                        </tbody>
+                        </table>
+                        <a href="acesso.html"><button type="button" class="btn btn-lg btn-danger btn-block" value="Cancelar" onclick="redirecionamento();">Voltar</button></a>
+                    </form>
+	            </div>
+            </div>
+        </div>
+        <script src="bootstrap/js/jquery-3.4.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+        <script src="bootstrap/js/javascriptsistema.js"></script>
+    </body>
+</html>
